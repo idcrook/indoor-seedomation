a747b49307962130ac4f4747a5bc34596c98b23f
David Crook <idcrook@users.noreply.github.com>
2023-03-03 18:20:12 -0700

    an example discovery/mapping script

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # indoor-seedomation
 A python-based indoor seedling starter monitor and control
+
+
+
+```shell
+# Install a python3 dev setup and other libraries
+sudo apt install -y python3-pip python3-venv build-essential \
+    python3-setuptools python3-wheel git
+
+# place a clone of this repo in ~/projects/
+mkdir ~/projects
+cd ~/projects
+
+git clone https://github.com/idcrook/indoor-seedomation.git
+cd indoor-seedomation
+```
+
+
+### Install
+
+
+```
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --no-cache-dir -r requirements.txt
+```
+
+From the [pywemo docs](https://github.com/pywemo/pywemo)
+
+```python
+>>> import pywemo
+>>> devices = pywemo.discover_devices()
+>>> print(devices)
+[<WeMo Insight "AC Insight">]
+
+>>> devices[0].toggle()
+```
+
+> For advanced usage, the `device.explain()` method will print all known actions that the device reports to PyWeMo.
+
---
+++ discovery.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import pywemo
+import re
+
+devices_of_interest = ('Bedroom 2 Plant Light 2', 
+    'Bedroom 2 Plant Heater')
+
+devices = pywemo.discover_devices()
+print(devices)
+for device in devices:
+    print (device.name, end = " ")
+print()
+
+device_mapping = {}
+
+for device in devices:
+    name = device.name
+    #print(name)
+    #print('state = ', device.get_state())
+    metainfo = str(device.metainfo.GetMetaInfo())
+    deviceinfo = str(device.deviceinfo.GetDeviceInformation())
+    if hasattr(device, "GetSmartDevInfo"):
+        print(device.basicevent.GetSmartDevInfo())
+    #print(device.deviceinfo.controlURL)
+    ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', device.deviceinfo.controlURL )[0]
+    url = pywemo.setup_url_for_address(ip)
+    if name in devices_of_interest:
+        device_mapping [name] = url 
+        #print(name, metainfo, deviceinfo)
+
+print(device_mapping)
+
+for name, url in device_mapping.items(): 
+    print (name, url)
